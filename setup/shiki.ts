7db33e64bd012ce3184ef91848f36431f7b6e7fa
yulei <zc_1994@163.com>
2024-02-28 11:27:37 +0800

    first commit

---
+++ setup/shiki.ts
@@ -0,0 +1,10 @@
+import { defineShikiSetup } from '@slidev/types'
+
+export default defineShikiSetup(() => {
+  return {
+    themes: {
+      dark: 'vitesse-dark',
+      light: 'vitesse-light',
+    },
+  }
+})
